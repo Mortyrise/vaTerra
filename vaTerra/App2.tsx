c6257718cb9681497a1fdf7704019cb2fd648f05
Siebe88 <siebe.kylstra@gmail.com>
2022-06-20 11:19:55 +0200

    REFACTOR: Everything TS

--- vaTerra/App2.tsx
+++ vaTerra/App2.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import LoginButton from './components/LoginButton';
+// import LoginButton from './components/LoginButton';
 const image = require('./assets/backgroundCover.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground style={styles.image} resizeMode="cover" source={image}>
-        <Text style={styles.text}>vaTerra</Text>
-        <LoginButton style={styles.loginbutton} />
+        {/* <Text style={styles.text}>vaTerra</Text> */}
+        {/* <LoginButton style={styles.loginbutton} /> */}
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
